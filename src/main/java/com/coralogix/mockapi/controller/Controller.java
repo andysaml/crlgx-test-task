08ffde6e69cba89848ba44a3c6645b29e326d859
Andy Saml <lander@i.ua>
2018-04-26 13:17:12 +0300

    logs added

--- src/main/java/com/coralogix/mockapi/controller/Controller.java
+++ src/main/java/com/coralogix/mockapi/controller/Controller.java
@@ -3,6 +3,7 @@ package com.coralogix.mockapi.controller;
 import com.coralogix.mockapi.exceptions.InvalidLogEntryException;
 import com.coralogix.mockapi.model.LogEntries;
 import com.coralogix.mockapi.model.LogEntry;
+import org.apache.log4j.Logger;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,23 +12,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
-
 @RestController
 public class Controller {
+
+    final static Logger logger = Logger.getLogger(Controller.class);
 
     @RequestMapping(path = "/api/v1/logs", method = RequestMethod.POST)
     public ResponseEntity<Void> getLogEntry(@RequestBody LogEntries logEntries) throws InvalidLogEntryException {
 
-        if (logEntries.getPrivateKey() instanceof UUID)
-            System.out.println("Got new logs: UUID - ok, app name: " + logEntries.getApplicationName());
-        else {
-            throw new InvalidLogEntryException("LogEntry UUID is wrong");
-        }
-        for (LogEntry le : logEntries.getLogEntries() ) {
-            System.out.println(le.getTimestamp() + ", " + le.getSeverity() + ", " +
-                    le.getText() + ", " + le.getCategory() + ", " + le.getClassName() + ", " +
-                    le.getMethodName()+ ", " + le.getThreadId());
+        try {
+            logger.info("Got log from:" + logEntries.getApplicationName());
+            for (LogEntry le : logEntries.getLogEntries()) {
+                logger.info("Logs: " + le.getTimestamp() + ", " + le.getSeverity() + ", " +
+                        le.getText() + ", " + le.getCategory() + ", " + le.getClassName() + ", " +
+                        le.getMethodName() + ", " + le.getThreadId());
+            }
+        } catch (Exception e) {
+            logger.error("Error: ", e);
+            throw new InvalidLogEntryException("Log Entry is invalid");
         }
         HttpHeaders headers = new HttpHeaders();
         return new ResponseEntity<>(headers, HttpStatus.OK);
